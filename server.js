00a609042ee5d69e2202086a1638837d677d99b1
Brian Thomas <Brianthomas571@gmail.com>
2020-06-15 22:31:53 -0500

    adding starter routes

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const app = express();
 
 app.get("/", (req, res) => res.json({ message: "Contact Management API" }));
 
+// HTTP Requests: GET(getting data from server), POST(Submitting info to server), PUT(Update info on server), DELETE(Delete info on server)
+
+// define routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
