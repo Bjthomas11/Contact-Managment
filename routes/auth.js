852a4d1d15b640132f60748e91514f7cbd38e44a
Brian Thomas <Brianthomas571@gmail.com>
2020-06-17 22:06:32 -0500

    adding auth route and payload

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,13 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/User");
+
 // auth routes
 // registered user routes
-const express = require("express");
 const router = express.Router();
 
 // @route  GET api/auth
@@ -13,9 +20,56 @@ router.get("/", (req, res) => {
 // @route  POST api/auth
 // @desc   Auth user & get token
 // @access  Public
-router.post("/", (req, res) => {
-  res.send("Log in user");
-});
+router.post(
+  "/",
+  [
+    check("email", "email is required").isEmail(),
+    check("password", "password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email });
+      if (!user) {
+        return res.status(400).json({ message: "Invalid Creds" });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: "Invalid Creds" });
+      }
+
+      //   payload to send with token
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (error, token) => {
+          if (error) {
+            throw error;
+          }
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 // export router
 module.exports = router;
