270aaf329bcec6b313805717439ac7f8c0d3bb55
Brian Thomas <Brianthomas571@gmail.com>
2020-06-17 21:36:25 -0500

    added bcrypt for adding a user

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 // registered user routes
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
 
@@ -13,16 +14,41 @@ router.post(
   [
     check("name", "name is required").not().isEmpty(),
     check("email", "email is required").isEmail(),
-    check("password", "password is required, 6 more characters").isLength({
+    check("password", "Password must be 6 or more characters").isLength({
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("passed");
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res.status(400).json({ message: "User already exist" });
+      }
+      user = new User({
+        name: name,
+        email: email,
+        password: password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      //   give hash version of password
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send("Added");
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
