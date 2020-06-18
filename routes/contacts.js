6e003656461d465ad442f0d845d419df217b0bed
Brian Thomas <Brianthomas571@gmail.com>
2020-06-17 22:51:12 -0500

    added PUT, DELETE contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -58,15 +58,69 @@ router.post(
 // @route  PUT api/contacts/:id
 // @desc  Update contact
 // @access  Private
-router.put("/:id", (req, res) => {
-  res.send("Updates a contact");
+router.put("/:id", auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  // build contact object based on whats submitted
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    //   finds contact by id
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      return res.status(404).json({ message: "contact not found" });
+    }
+
+    // make user owns contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: contactFields,
+      },
+      {
+        new: true,
+      }
+    );
+
+    res.json(contact);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route  DELETE api/contacts/:id
 // @desc  Delete contact
 // @access  Private
-router.delete("/:id", (req, res) => {
-  res.send("Delete a contact");
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    //   finds contact by id
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      return res.status(404).json({ message: "contact not found" });
+    }
+
+    // make user owns contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+
+    await Contact.findByIdAndRemove(req.params.id);
+    res.json({
+      message: "contact deleted",
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // export router
