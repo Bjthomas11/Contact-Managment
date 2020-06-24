7ea5f32e966780ae1d7e358952e6311fa582ad1c
Brian Thomas <Brianthomas571@gmail.com>
2020-06-23 23:12:18 -0500

    added contacts context with ui changes/animations

--- client/src/components/pages/home/home.component.jsx
+++ client/src/components/pages/home/home.component.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Contacts from "../../contacts/contacts.component";
+import ContactForm from "../../forms/contact-form.component";
+import ContactFilter from "../../contacts/contact-filter.component";
 
 const Home = () => {
   return (
     <div className="grid-2">
-      <div>{/* contact form */}</div>
+      <ContactForm />
       <div>
+        <ContactFilter />
         <Contacts />
       </div>
     </div>
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-import uuid from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import ContactContext from "./contactContext";
 import contactReducer from "./contactReducer";
 import {
@@ -13,46 +13,75 @@ import {
 } from "../types";
 
 const ContactState = (props) => {
-  const initalState = {
+  const initialState = {
     contacts: [
       {
         id: 1,
-        name: "Brian Thomas",
-        email: "brian@gmail.com",
-        phone: "111-111-1111",
-        type: "personal",
-      },
-      {
-        id: 2,
         name: "Brian 2nd",
         email: "brian2nd@gmail.com",
         phone: "111-111-1111",
         type: "professional",
       },
+      {
+        id: 2,
+        name: "Brian 3rd",
+        email: "brian3nd@gmail.com",
+        phone: "111-111-1111",
+        type: "personal",
+      },
     ],
+    current: null,
+    filtered: null,
   };
-  const [state, dispatch] = useReducer(contactReducer, initalState);
+  const [state, dispatch] = useReducer(contactReducer, initialState);
   const { children } = props;
-  const { contacts } = state;
+  const { contacts, current, filtered } = state;
 
   // add contact
+  const addContact = (contact) => {
+    contact.id = uuidv4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
 
   // delete contact
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
 
   // set current contact
-
+  const setCurrent = (contact) => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
   // clear contact
-
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
   // update contact
-
+  const updateContact = (contact) => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
   // filter contacts
-
+  const filterContacts = (text) => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
   // clear filter contacts
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
 
   return (
     <ContactContext.Provider
       value={{
         contacts: contacts,
+        addContact,
+        deleteContact,
+        current: current,
+        setCurrent,
+        clearCurrent,
+        updateContact,
+        filterContacts,
+        clearFilter,
+        filtered: filtered,
       }}
     >
       {children}
